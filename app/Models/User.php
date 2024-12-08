e1b2965bb9d6be77eaa8128bc446c5e8b63d7121
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2024-12-08 13:35:11 +0700

    add db schema for managing restaurant by the admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\RoleName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +47,53 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->hasRole(RoleName::ADMIN);
+    }
+
+    public function isVendor(): bool
+    {
+        return $this->hasRole(RoleName::VENDOR);
+    }
+
+    public function isStaff(): bool
+    {
+        return $this->hasRole(RoleName::STAFF);
+    }
+
+    public function isCustomer(): bool
+    {
+        return $this->hasRole(RoleName::CUSTOMER);
+    }
+
+    public function hasRole(RoleName $role): bool
+    {
+        return $this->roles()->where("name", $role->value)->exists();
+    }
+
+    public function permissions(): array
+    {
+        return $this->roles()->with("permissions")->get()
+            ->map(function ($role) {
+                return $role->permissions->pluck("name");
+            })->flatten()->values()->unique()->toArray();
+    }
+
+    public function hasPermission(string $permission): bool
+    {
+        return in_array($permission, $this->permissions(), true);
+    }
+
+    public function restaurant(): HasOne
+    {
+        return $this->hasOne(Restaurant::class, "owner_id");
     }
 }
